29d0934f867e91278ab841224ec826521905db96
Goal651 <bugiriwilson651@gmail.com>
2025-12-08 20:52:37 +0200

    feat: Initialize Django project with virtual environment and remove frequency encoding script.

---
+++ django_tutorial/project_creation.sh
@@ -0,0 +1,16 @@
+# Install django global
+pip install django
+
+# Creation of environment to be used
+python -m venv env
+
+# activate environment
+source env/bin/activate
+
+# 2 ways of project creation
+# First one 
+mkdir project_name
+django-admin startproject project project_name 
+
+# Second 
+django-admin startproject test_project
--- label_encoding.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-data=["small","medium","large","small","large"]
-
-label_encoder=LabelEncoder( )
-encoded_data=label_encoder.fit_transform(data)
-
-
-print(encoded_data)
